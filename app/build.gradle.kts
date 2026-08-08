20a20056ac203572e0d3b81c065d1c6bea953f7a
Medskiyyy <bulek6878@gmail.com>
2026-08-08 07:39:10 +0700

    bump: upgrade version to v3.2.0 (versionCode 8) with smart mother message generator

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.mother.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "3.1.0"
+        versionCode = 8
+        versionName = "3.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
